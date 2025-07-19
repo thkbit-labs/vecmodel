4d209ddf5c79832ee0be4197535e96e480a38c8d
HungLM1506 <leminhhung933@gmail.com>
2025-07-19 19:07:37 +0700

    feat: init-project

--- src/vecmodel/__init__.py
+++ src/vecmodel/__init__.py
@@ -1,3 +1,6 @@
 from __future__ import annotations
 
 from vecmodel.__version__ import __version__
+
+
+__all__ = ["__version__"]
--- src/vecmodel/__version__.py
+++ src/vecmodel/__version__.py
@@ -1,1 +1,4 @@
-__version__= "0.0.0"
+from __future__ import annotations
+
+
+__version__ = "0.0.0"
